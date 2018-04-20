66455a67e374d24d72222417d739d0442b176369
LuisBGit <l.borja5678@gmail.com>
2018-04-21 07:17:00 +1200

    Cleaning up folders

--- V4/IRLongRange.cpp
+++ V4/IRLongRange.cpp
@@ -1,11 +1,11 @@
-#include "IRShortRange.h"
+#include "IRLongRange.h"
 
 
-void IRShort::setupIR(int AnalogIn) {
-  this->pin = AnalogIn;
+void IRLong::setupIR(int AnalogIn) {
+    this->pin = AnalogIn;
 }
 
-float IRShort::readSensor() {
+float IRLong::readSensor() {
   if(arrayPos == 5)
   {
     arrayPos = 0;
@@ -14,16 +14,16 @@ float IRShort::readSensor() {
    this->filterStore[this->arrayPos] = result;
    float mean = ((this->filterStore[0]) + (this->filterStore[1]) + (this->filterStore[2]) + (this->filterStore[3]) + (this->filterStore[4])) /5 ;
    this->arrayPos++;
-   return multiMap(mean, this->in, this->out, 14);
+   return multiMap(mean, this->in, this->out, 31);
    
 }
 
 
-float IRShort::multiMap(int val, float* _in, float* _out, uint8_t size)
+float IRLong::multiMap(int val, float* _in, float* _out, uint8_t size)
 {
   // take care the value is within range
   // val = constrain(val, _in[0], _in[size-1]);
-  if (val >= _in[0]) return _out[0];//chanmge to flag
+  if (val >= _in[0]) return _out[0];
   if (val <= _in[size-1]) return _out[size-1];
 
   // search right interval
@@ -34,5 +34,5 @@ float IRShort::multiMap(int val, float* _in, float* _out, uint8_t size)
   if (val == _in[pos]) return _out[pos];
 
   // interpolate in the right segment for the rest
-  return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1]  + 1;
+  return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
 }
--- V4/IRShortRange.cpp
+++ V4/IRShortRange.cpp
@@ -1,11 +1,11 @@
-#include "IRLongRange.h"
+#include "IRShortRange.h"
 
 
-void IRLong::setupIR(int AnalogIn) {
-    this->pin = AnalogIn;
+void IRShort::setupIR(int AnalogIn) {
+  this->pin = AnalogIn;
 }
 
-float IRLong::readSensor() {
+float IRShort::readSensor() {
   if(arrayPos == 5)
   {
     arrayPos = 0;
@@ -14,16 +14,16 @@ float IRLong::readSensor() {
    this->filterStore[this->arrayPos] = result;
    float mean = ((this->filterStore[0]) + (this->filterStore[1]) + (this->filterStore[2]) + (this->filterStore[3]) + (this->filterStore[4])) /5 ;
    this->arrayPos++;
-   return multiMap(mean, this->in, this->out, 31);
-   
+   return multiMap(mean, this->in, this->out, 14);
+
 }
 
 
-float IRLong::multiMap(int val, float* _in, float* _out, uint8_t size)
+float IRShort::multiMap(int val, float* _in, float* _out, uint8_t size)
 {
   // take care the value is within range
   // val = constrain(val, _in[0], _in[size-1]);
-  if (val >= _in[0]) return _out[0];
+  if (val >= _in[0]) return _out[0];//chanmge to flag
   if (val <= _in[size-1]) return _out[size-1];
 
   // search right interval
@@ -34,5 +34,5 @@ float IRLong::multiMap(int val, float* _in, float* _out, uint8_t size)
   if (val == _in[pos]) return _out[pos];
 
   // interpolate in the right segment for the rest
-  return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
+  return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1]  + 1;
 }
